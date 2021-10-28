1cabdcdcbe91dff5283cecdefce7926e4020db6c
jhkim593 <fpdlwjzlr@naver.com>
2021-10-28 15:10:40 +0900

    feat : index page 및  결제 , 취소 , 조회 update

--- payfront/routes/index.js
+++ payfront/routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/pay', function(req, res, next) {
+  res.render('pay', { title: 'Express' });
+});
+
+router.get('/cancelPay', function(req, res, next) {
+  res.render('cancelPay', { title: 'Express' });
+});
+
+router.get('/getPay', function(req, res, next) {
+  res.render('getPay', { title: 'Express' });
+});
+
+
+
 module.exports = router;
